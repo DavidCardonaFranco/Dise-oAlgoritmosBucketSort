a76b611134823740db8891f38f42893c4d33da78
DavidCardonaFranco <73562062+DavidCardonaFranco@users.noreply.github.com>
2022-02-14 08:04:53 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
-def bucket_sort(arreglo):
+#Importanción de recursos de una Interfaz de línea de comandos y un invocable.
+from timeit import timeit
 
+def bucket_sort(arreglo):
+    #Inicialización de variables
     mayor = max(arreglo)
     tamaño = len(arreglo)
     size = mayor / tamaño
-
     buckets = [[] for _ in range(tamaño)]
 
+    #Establecer el rango
     for i in range(tamaño):
         j = int(arreglo[i] / size)
         if j != tamaño:
@@ -13,9 +16,11 @@ def bucket_sort(arreglo):
         else:
             buckets[tamaño - 1].append(arreglo[i])
 
+    #Ordenar todos los elementos de cada cubeta
     for i in range(tamaño):
         insertion_sort(buckets[i])
 
+    #Ordenar cada elementos entre cubetas
     resultado = []
     for i in range(tamaño):
         resultado = resultado + buckets[i]
@@ -38,3 +43,4 @@ arreglo = [int(x) for x in arreglo]
 lista_ordenada = bucket_sort(arreglo)
 print('Lista Ordenada: ', end='')
 print(lista_ordenada)
+print(f"{timeit()} segundos")
